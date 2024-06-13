564764a2eba22960c0e2910bec05f9c05b2a5375
Yassin ashraf <yassinyoyoashraf2018@gmail.com>
2024-06-13 22:18:56 +0300

    session 2 task1

---
+++ tasks/session_2/task1.py
@@ -0,0 +1,5 @@
+import task1Module
+
+githubURl =task1Module.githuburl
+
+task1Module.firefoxTakeURL(githubURl)
---
+++ tasks/session_2/task1Module.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+githuburl="https://github.com/yassin448/embedded-linux/tree/main/tasks_1"
+
+
+def firefoxTakeURL(githuburl):
+    return  webbrowser.open(url=githuburl)
